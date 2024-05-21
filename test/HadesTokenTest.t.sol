99dd1e98342571e2eb3a572b5d2755204486ba30
Taki Baker <alyasritaki@gmail.com>
2024-05-21 12:43:16 -0700

    Finished tests

--- test/HadesTokenTest.t.sol
+++ test/HadesTokenTest.t.sol
@@ -5,6 +5,10 @@ import {Test} from "forge-std/Test.sol";
 import {DeployHadesToken} from "../script/DeployHadesToken.s.sol";
 import {HadesToken} from "../src/HadesToken.sol";
 
+interface MintableToken {
+    function mint(address, uint256) external;
+}
+
 contract HadesTokenTest is Test {
     HadesToken public hadesToken;
     DeployHadesToken public deployer;
@@ -22,7 +26,32 @@ contract HadesTokenTest is Test {
         hadesToken.transfer(twix, STARTING_BALANCE);
     }
 
+    function testInitialSupply() public {
+        assertEq(hadesToken.totalSupply(), deployer.INITIAL_SUPPLY());
+    }
+
     function testBobBalance() public {
         assertEq(STARTING_BALANCE, hadesToken.balanceOf(twix));
+    }
+
+    function testUsersCantMint() public {
+        vm.expectRevert();
+        MintableToken(address(hadesToken)).mint(address(this), 1);
+    }
+
+    function testAllowancesWorks() public {
+        uint256 intialAllowance = 1000;
+
+        vm.prank(twix);
+        hadesToken.approve(carti, intialAllowance);
+
+        uint256 transferAmount = 500;
+
+        vm.prank(carti);
+        hadesToken.transferFrom(twix, carti, transferAmount);
+
+        // Assert
+        assertEq(hadesToken.balanceOf(carti), transferAmount);
+        assertEq(hadesToken.balanceOf(twix), STARTING_BALANCE - transferAmount);
     }
 }
